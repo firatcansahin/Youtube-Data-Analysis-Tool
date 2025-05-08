0fa25cfac35bc228c73ca78405d942c418eb7f9b
Firatcan Sahin <firatcansahin@gmail.com>
2025-05-08 15:55:21 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const axios = require('axios');
 const cors = require('cors');
+const path = require('path'); // added for vercel
 require('dotenv').config();
 
 const app = express();
